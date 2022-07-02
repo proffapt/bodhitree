0bb85f0c5a0b6153de378ac8741fb18b05d28517
Arpit Bhardwaj <86282911+proffapt@users.noreply.github.com>
2022-07-02 09:40:41 +0200

    Add files via upload

---
+++ cwh_java_code/088_089-collections_framework.java
@@ -0,0 +1,18 @@
+// Collections FrameWork
+/*
+	Paki pakayi khichdi.. basically the STL(as in cpp) of JAVA.
+	It provides classes.. interfaces.. and some static methods(sorting, searching etc..).. for the programmer to write code quickly and efficiently.
+	***** found inside java.util as java.util.Collections *****
+*/
+import java.util.Collections;
+// To call particular member of collection set explicitly
+/*
+ import java.util.ArrayList;
+ import java.util.Set;
+ import java.util.TreeSet;
+*/
+
+class Main{
+	public static void main(String[] args){
+	}
+}
