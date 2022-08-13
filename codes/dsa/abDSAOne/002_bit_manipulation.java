0280be72ada57d72bcc80309d45c6cd89c29f71e
proffapt <proffapt@pm.me>
2022-08-13 11:28:40 +0530

    updating via gsync-syncscript by @proffapt

---
+++ codes/dsa/abDSAOne/002_bit_manipulation.java
@@ -0,0 +1,18 @@
+class Main{
+	public static void main(String[] args){
+		// finding odd / even	
+		// int n=13;
+		// if (n&1==0) System.out.println("Even");
+		// else System.out.println("Odd");
+
+		// swapping two numbers
+		int a=5, b=7;
+		System.out.println("Before swapping");
+		System.out.println("a="+a+"and b="+b);
+		a=a^b;
+		b=a^b;
+		a=a^b;
+		System.out.println("After swapping");
+		System.out.println("a="+a+"and b="+b);
+	}
+}
