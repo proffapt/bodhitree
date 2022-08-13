e30ae1b71c628e1b542e52c53d1663259d6e2355
proffapt <proffapt@pm.me>
2022-08-13 11:44:30 +0530

    updating via gsync-syncscript by @proffapt

--- codes/dsa/abDSAOne/003_bit_masking.java
+++ codes/dsa/abDSAOne/003_bit_masking.java
@@ -6,5 +6,11 @@ class Main{
 		int mask=1<<i;	
 		if((n&mask)==0) System.out.println("The "+i+"th bit was: 0");
 		else System.out.println("The "+i+"th bit was: 1");
+		// All the bits became 0, because of '&' with 0 and the one to be found
+		// got '&' with 1 and we can determine easily..
+		// if it was 0 > 1&0=0 > whole number 0 > thus it was 0
+		// else it will be a non-zero number > thus it was 1
+
+		// Setting ith bit
 	}
 }
