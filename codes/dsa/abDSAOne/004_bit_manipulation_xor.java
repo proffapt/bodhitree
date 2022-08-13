d34876eabac7631da80eadac5bbc142f5dc797d4
proffapt <proffapt@pm.me>
2022-08-13 17:17:15 +0530

    updating via gsync-syncscript by @proffapt

--- codes/dsa/abDSAOne/004_bit_manipulation_xor.java
+++ codes/dsa/abDSAOne/004_bit_manipulation_xor.java
@@ -13,8 +13,8 @@ class Main{
 			Find the only non-repeating element in an array where every element repeats twice.
 		*/
 		int[] arr={5, 4, 1, 4, 3, 5, 1};
-		for(int i: arr) System.out.print(i+"");
-		System.out.println("");
+		for(int i: arr) System.out.print(i+" ");
+		System.out.println();
 		int res=0;
 		/*
 		for(int i=0; i<arr.length; i++){
@@ -30,8 +30,8 @@ class Main{
 			Find the two non-repeating elements in an array where every element repeats twice.
 		*/
 		int[] arr1={5, 4, 1, 4, 3, 5, 1, 2};
-		for(int i: arr1) System.out.print(i+"");
-		System.out.println("");
+		for(int i: arr1) System.out.print(i+" ");
+		System.out.println();
 		res=0;
 		for(int i: arr1) res^=i; // will yield res=3^2
 		// now have to separate the array into two parts; based on the logic that..
@@ -54,8 +54,8 @@ class Main{
 		   Find the only non-repeating element in an array where every element is repeating 3 times.
 		*/
 		int arr2[]={2, 2, 1, 5, 1, 1, 2, 3 ,3 , 3};
-		for(int i: arr2) System.out.print(i+"");
-		System.out.println("");
+		for(int i: arr2) System.out.print(i+" ");
+		System.out.println();
 		int[] bit32=new int[32];
 		for(int i=0; i<32; i++){
 			mask=1<<i;
