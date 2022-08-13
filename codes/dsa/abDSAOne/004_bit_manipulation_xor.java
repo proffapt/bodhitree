9e691cf6dcd9f0bc769a1796e6fe9e6184a60680
proffapt <proffapt@pm.me>
2022-08-13 14:42:39 +0530

    updating via gsync-syncscript by @proffapt

---
+++ codes/dsa/abDSAOne/004_bit_manipulation_xor.java
@@ -0,0 +1,20 @@
+// Properties of XOR
+/*
+	a^a=0
+	0^b=b
+*/
+class Main{
+	public static void main(String[] args){
+		// One application was for finding how many bits to change to change a number to other.	
+		// TWO:
+		/*
+			Find the only non-repeating element in an array where every element repeats twice.
+		*/
+		int arr[]={5, 4, 1, 4, 3, 5, 1};
+		int res=0;
+		for(int i=0; i<arr.length; i++){
+			res=res^arr[i];
+		}
+		System.out.println("The unique number is: "+res);
+	}
+}
