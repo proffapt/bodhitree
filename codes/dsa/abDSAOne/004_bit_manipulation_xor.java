f9aff7789059613b8e4f0c53c39aee0e561dd215
proffapt <proffapt@pm.me>
2022-08-13 14:44:23 +0530

    updating via gsync-syncscript by @proffapt

--- codes/dsa/abDSAOne/004_bit_manipulation_xor.java
+++ codes/dsa/abDSAOne/004_bit_manipulation_xor.java
@@ -12,9 +12,13 @@ class Main{
 		*/
 		int arr[]={5, 4, 1, 4, 3, 5, 1};
 		int res=0;
+		/*
 		for(int i=0; i<arr.length; i++){
 			res=res^arr[i];
 		}
+		*/
+		// Doing it the java way
+		for(int i: arr) res^=i;
 		System.out.println("The unique number is: "+res);
 	}
 }
