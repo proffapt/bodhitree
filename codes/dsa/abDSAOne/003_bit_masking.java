24b3b8bd093e1aa3fef4834468445b57bbc54475
proffapt <proffapt@pm.me>
2022-08-13 11:40:18 +0530

    updating via gsync-syncscript by @proffapt

---
+++ codes/dsa/abDSAOne/003_bit_masking.java
@@ -0,0 +1,8 @@
+class Main{
+	public static void main(String[] args){
+		int n=309;
+		int i=5; // counting from right.. starting with 0
+	 	// finding ith bit in the binary for of the number
+		int mask=1<<i;	
+	}
+}
