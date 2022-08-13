e6ca8bada21d507916b5b083af9d2d0bc370f9b9
proffapt <proffapt@pm.me>
2022-08-13 11:42:28 +0530

    updating via gsync-syncscript by @proffapt

--- codes/dsa/abDSAOne/003_bit_masking.java
+++ codes/dsa/abDSAOne/003_bit_masking.java
@@ -4,5 +4,7 @@ class Main{
 		int i=5; // counting from right.. starting with 0
 	 	// finding ith bit in the binary for of the number
 		int mask=1<<i;	
+		if((n&mask)==0) System.out.println("The "+i+"th bit was: 0");
+		else System.out.println("The "+i+"th bit was: 1");
 	}
 }
