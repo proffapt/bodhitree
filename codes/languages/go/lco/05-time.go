942842f5b7d136123eef34f094c511bf85b367b2
proffapt <proffapt@pm.me>
2022-10-15 19:29:22 +0530

    update

--- codes/languages/go/lco/05-time.go
+++ codes/languages/go/lco/05-time.go
@@ -8,6 +8,6 @@ import (
 func main() {
 	currentTime := time.Now()
 	fmt.Println(currentTime)
-	fmtdCurrentTime := currentTime.Format("01-02-2006 15:04:05")
+	fmtdCurrentTime := currentTime.Format("01/02/2006 15:04:05")
 	fmt.Print(fmtdCurrentTime)
 }
