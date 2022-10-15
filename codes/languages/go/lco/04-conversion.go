7d4ab19aa0d5266f54feb526237d665147c1324f
proffapt <proffapt@pm.me>
2022-10-15 18:30:39 +0530

    update

--- codes/languages/go/lco/04-conversion.go
+++ codes/languages/go/lco/04-conversion.go
@@ -14,13 +14,13 @@ func main() {
 	fmt.Print("Rate my github profile in between 1 to 5: ")
 	rating, _ := reader.ReadString('\n')
 
-	fmt.Print("Thanks for rating my profile, ", rating)
+	fmt.Println("Thanks for rating my profile, ", rating)
 
 	// numRating := rating+1 --> it's all good, but what if we get an error? How will you handle it?
 	numRating, err := strconv.ParseFloat(strings.TrimSpace(rating), 64)
 	if err != nil {
 		panic(err)
 	} else {
-		fmt.Print("Thanks for rating my profile, ", numRating+1)
+		fmt.Println("Thanks for rating my profile, ", numRating+1)
 	}
 }
