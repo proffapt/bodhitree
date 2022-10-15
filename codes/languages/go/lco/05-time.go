f3125980a3b659a3866547d1bb978eda88521d83
proffapt <proffapt@pm.me>
2022-10-15 19:32:32 +0530

    update

--- codes/languages/go/lco/05-time.go
+++ codes/languages/go/lco/05-time.go
@@ -11,6 +11,6 @@ func main() {
 	fmtdCurrentTime := currentTime.Format("Monday 01/02/2006 15:04:05")
 	fmt.Print(fmtdCurrentTime)
 
-	myDate := time.Date(2021, time.August, 11, 23, 9, 0, time.UTC)
+	myDate := time.Date(2021, time.August, 11, 23, 4, 9, 0, time.UTC)
 	fmt.Print(myDate)
 }
