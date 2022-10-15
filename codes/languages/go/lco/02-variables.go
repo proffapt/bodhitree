c0d8962f33062624efa85cccc8dcea4472413fae
proffapt <proffapt@pm.me>
2022-10-15 14:56:22 +0530

    update

--- codes/languages/go/lco/02-variables.go
+++ codes/languages/go/lco/02-variables.go
@@ -6,4 +6,8 @@ func main() {
 	var username string = "proffapt"
 	fmt.Println(username)
 	fmt.Printf("Variable is of type: %T\n", username)
+
+	var isLoggedIn bool = true
+	fmt.Println(isLoggedIn)
+	fmt.Printf("Variable is of type: %T\n", isLoggedIn)
 }
