2eaa1a0eb732a0e2185172876a073265efebc8ca
proffapt <proffapt@pm.me>
2022-10-15 20:04:46 +0530

    update

--- codes/languages/go/lco/07-array.go
+++ codes/languages/go/lco/07-array.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	
+	var array [4]string
+	array[0] = "apple"
+	array[1] = "microsoft"
+	array[2] = "samsung"
+	array[3] = "tesla"
+
+	fmt.Println(array)
 }
