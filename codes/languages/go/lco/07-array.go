574a3002deda868814d2aa6224c32ba4c4bbe12e
proffapt <proffapt@pm.me>
2022-10-15 20:09:02 +0530

    update

--- codes/languages/go/lco/07-array.go
+++ codes/languages/go/lco/07-array.go
@@ -6,9 +6,12 @@ func main() {
 	var array [4]string
 	array[0] = "apple"
 	array[1] = "microsoft"
-	array[3] = "tesla"
+	array[3] = "tesla" // notice an extra space is there between tesla and microsoft - to indicate empty element
 
 	fmt.Println(array[3])
 	fmt.Println(array)
 	fmt.Println(len(array))
+
+	var myArray = [3]string{"fuck", "your", "momma"}
+	fmt.Println(myArray)
 }
