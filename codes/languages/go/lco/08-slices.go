2437f51250f24a91454a4e4951f1c2db03c74359
proffapt <proffapt@pm.me>
2022-10-15 21:39:25 +0530

    update

--- codes/languages/go/lco/08-slices.go
+++ codes/languages/go/lco/08-slices.go
@@ -5,8 +5,17 @@ import "fmt"
 func main() {
 	// arrays without size-declration are slices
 	ms := []int{1, 2, 3, 4}
+	// can also be done using
+	/*
+		var ms []int
+		ms[0] = 1
+		ms[1] = 2
+		ms[2] = 3
+		ms[3] = 4
+	*/
 	fmt.Println(ms)
 	// appending
 	ms = append(ms, 5, 6)
 	fmt.Println(ms)
+	fmt.Println(ms[4])
 }
