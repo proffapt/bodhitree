e572b3f7352d1e1f9a3b919e0a89d0eead8588cd
proffapt <proffapt@pm.me>
2022-10-15 18:12:20 +0530

    update

--- codes/languages/go/lco/03-userinput.go
+++ codes/languages/go/lco/03-userinput.go
@@ -10,7 +10,7 @@ func main() {
 	// creating the reference to our reader
 	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Print("Enter the rating for my github page: ")
+	fmt.Print("Enter the rating for my github page:")
 	input, _ := reader.ReadString('\n')
 
 	fmt.Println("You gave us rating of ", input)
