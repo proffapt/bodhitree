c5d1cd767d2268ab465fa85d43ca77949192a342
proffapt <proffapt@pm.me>
2022-10-15 14:52:13 +0530

    update

--- codes/languages/go/lco/02-variables.go
+++ codes/languages/go/lco/02-variables.go
@@ -3,6 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("gonna learn about variables")
-	var username string = "Fuck You"
+	var username string = "proffapt"
+	fmt.Println(username)
 }
