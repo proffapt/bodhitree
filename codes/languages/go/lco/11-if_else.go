4851f28a3ed5846fc9a0b872f3ee4e7e327be917
proffapt <proffapt@pm.me>
2022-10-16 21:32:01 +0530

    update

--- codes/languages/go/lco/11-if_else.go
+++ codes/languages/go/lco/11-if_else.go
@@ -11,6 +11,17 @@ func main() {
 	} else {
 		result = "Hey, you so active give some more moniii"
 	}
-
+	/*
+		Some words about the looks of the loop
+		YOU CAN NOOOT change the place of "{" or "else"
+		You wanna know why? ---> LEXER!!!
+		if you do that, if you really change the location, 
+		lexer will put a ';' there after "25" but when it encouters '{' it skips doing it.
+	*/
 	fmt.Println(result)
+
+	// alternate way, the go wayyy
+	if logincount := 96; logincount == 96 {
+		result = "You have acquired a holy state"
+	}
 }
