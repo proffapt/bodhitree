798b7d41ef0467aaf89a9bda44a58570f927d79e
proffapt <proffapt@pm.me>
2022-10-16 21:02:52 +0530

    update

--- codes/languages/go/lco/09-maps.go
+++ codes/languages/go/lco/09-maps.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	
+	languages := make(map[string]string)
+	languages["py"] = "python"
+	languages["cpp"] = "c++"
+	languages["sh"] = "bash"
+	languages["java"] = "java"
+
+	fmt.Println(languages)
 }
