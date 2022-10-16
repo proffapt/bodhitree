bbdd614bbfde06ecfbf1d2683e20902040fca158
proffapt <proffapt@pm.me>
2022-10-16 21:25:54 +0530

    update

--- codes/languages/go/lco/11-if_else.go
+++ codes/languages/go/lco/11-if_else.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
-func main() }
-	
-{}
+func main() {
+	loginCount := 69
+	var result string
+
+	if loginCount < 25 {
+		result = "Hey, why you inactive check this out!"
+	} else {
+		result = "Hey, you so active give some more moniii"
+	}
+
+	fmt.Println(result)
+}
