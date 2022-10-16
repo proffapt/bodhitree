9bc422fc932e594fbc929e188285e57bfdb4a9ad
proffapt <proffapt@pm.me>
2022-10-16 13:52:24 +0530

    update

--- codes/languages/go/lco/08-slices.go
+++ codes/languages/go/lco/08-slices.go
@@ -5,22 +5,67 @@ import "fmt"
 func main() {
 	// arrays without size-declration are slices
 	// ** Syntax 1
-	ms := []int{1, 2, 3, 4}
-	// can also be done using
 	/*
+		You have to initialise the slice while using this syntax
+		That is can NOT  be done using
+
 		var ms []int
 		ms[0] = 1
 		ms[1] = 2
 		ms[2] = 3
 		ms[3] = 4
+		|
+		--> Invalid
+
+		var ms []int{}
+		|
+		--> Invalid
+
+		>> Rather you will have to do this
+		ms := []int{}
 	*/
+	var ms = []int{1, 2, 3, 4}
+	// ms := []int{1, 2, 3, 4} --> WORKS too
 	fmt.Println(ms)
 	// appending
 	ms = append(ms, 5, 6)
 	fmt.Println(ms)
-	// ms[6] = 7
-	// fmt.Println(ms)
-	// ^ This will give error, only append will help you with that
+	/*
+		ms[6] = 7
+		fmt.Println(ms)
+		^ This will give error, only append will help you with that
+	*/
 	ms[5] = 7
 	fmt.Println(ms)
+	fmt.Printf("Datatype representation of Slice is = %T\n", ms)
+
+	// Slicing the slices
+	slice1 := append(ms[1:]) // start from the index 1
+	fmt.Println(slice1)
+	slice2 := append(ms[:4]) // go till the index 3
+	fmt.Println(slice2)
+	/*
+		The logic is like this
+		fucking_something[m:n] here [m:n] == [m, n), the last bound is not included!
+	*/
+	slice3 := append(ms[2:4]) // from index 2 to 3
+	fmt.Println(slice3)
+
+	// ** Syntax 2
+	/*
+		Utilising DMA
+
+		- Doesn't require you to initialise it
+		- Have to set a minimum of size to allocate during initialisation
+
+		So, it's like slices with minimum size.
+	*/
+	slice := make([]int, 4)
+	slice[0]=1
+	slice[1]=2
+	slice[2]=3
+	slice[3]=4
+	fmt.Println(slice)
+	slice = append(slice, 5)
+	fmt.Println(slice)
 }
