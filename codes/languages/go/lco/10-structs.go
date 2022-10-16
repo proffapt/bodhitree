4f902be294375da6640588f4e5456c31ee655bdc
proffapt <proffapt@pm.me>
2022-10-16 21:13:56 +0530

    update

---
+++ codes/languages/go/lco/10-structs.go
@@ -0,0 +1,15 @@
+package main
+// no inheritance concept in golang, neither super and parent.
+import "fmt"
+
+// Syntax of struct 
+type User struct { // FOCUS ON "User" that capital U has an importance, it means this struct User is publicly avaialable to use
+	Name string
+	Email string
+	Verified bool
+	Age int
+}
+
+func main() {
+	
+}
