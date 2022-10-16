122689976ebe56c813368f1165306828458d7ba5
proffapt <proffapt@pm.me>
2022-10-16 21:16:44 +0530

    update

--- codes/languages/go/lco/10-structs.go
+++ codes/languages/go/lco/10-structs.go
@@ -13,8 +13,8 @@ type User struct { // FOCUS ON "User" that capital U has an importance, it means
 func main() {
 	proffapt := User{"Arpit", "proffapt@pm.me", true, 20}
 	fmt.Println(proffapt)
+	fmt.Printf("%v\n", proffapt)
 
 	// for more detailed output
-	fmt.Printf("%v\n", proffapt)
 	fmt.Printf("%+v\n", proffapt)
 }
