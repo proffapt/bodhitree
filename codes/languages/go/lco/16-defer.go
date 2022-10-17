07f723c5431738c3de9a9b53793cf91c0e2646f0
proffapt <proffapt@pm.me>
2022-10-17 19:13:49 +0530

    update

--- codes/languages/go/lco/16-defer.go
+++ codes/languages/go/lco/16-defer.go
@@ -6,6 +6,7 @@ import "fmt"
 // that line of code with defer in the start is placed in the very end of the code, and hence is executed in the very end too.
 // Follows LIFO, logical hai.
 func main() {
+	// this will print the numbers in reverse order, and that too in the very end of program, as they were the first ones to go.. LIFO=FILO
 	for i:= 1; i<6; i++{
 		defer fmt.Println(i)
 	}
