67d24b412eb601e3a4c1adfa00ee1cb15fd8f891
proffapt <proffapt@pm.me>
2022-10-17 22:19:54 +0530

    update

---
+++ codes/languages/go/lco/18-urls.go
@@ -0,0 +1,15 @@
+package main
+
+// IMPORTANT ADVICE
+/*
+	Neither Read Respance nor Response.Write closes a connection.
+	That is, whenever you are making a request you have to make sure that 
+	neither your reader nor your writer is closing that request.
+*/
+import "fmt"
+
+const url string = "https://lco.dev/"
+
+func main)( {
+	
+})
