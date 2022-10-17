31153fc433c7ed1ebfccb19d038a22cb5a90ed5e
proffapt <proffapt@pm.me>
2022-10-17 17:49:14 +0530

    update

--- codes/languages/go/lco/13-for.go
+++ codes/languages/go/lco/13-for.go
@@ -5,4 +5,9 @@ import "fmt"
 func main() {
 	langs := []string{"c", "cpp", "python", "ruby", "rust"}
 	fmt.Println(langs)
+
+	// The RANGE for
+	for i := range langs {
+		fmt.Println(langs[i])
+	}
 }
