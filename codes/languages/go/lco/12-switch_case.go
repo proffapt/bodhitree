acb6297ab4cf1f8b95c4b8a41f387d41cb98009f
proffapt <proffapt@pm.me>
2022-10-17 17:16:09 +0530

    update

--- codes/languages/go/lco/12-switch_case.go
+++ codes/languages/go/lco/12-switch_case.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
-	
+	// generating random numbers
+	rand.Seed(time.Now().UnixNano()) // Best way for generating random number.. UnixNano() is a must!!
+
+	diceNumber := rand.Intn(6)+1
+
+	switch diceNumber {
+		case 1:
+			fmt.Println("Weee, it's a 1")
+		case 2:
+			fmt.Println("Weee, it's a 2")
+		case 3:
+			fmt.Println("Weee, it's a 3")
+		case 4:
+			fmt.Println("Weee, it's a 4")
+			fallthrough // will fall to the very next case only, NOOOT till the end!
+		case 5:
+			fmt.Println("Weee, it's a 5")
+		case 6:
+			fmt.Println("Weee, it's a 6")
+		default:
+			fmt.Println("What the fuck was that!?")
+	}
 }
