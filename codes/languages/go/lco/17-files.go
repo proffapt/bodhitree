53f94eca2ed9b454920b45fb4f6b23b626e21964
proffapt <proffapt@pm.me>
2022-10-17 21:54:54 +0530

    update

--- codes/languages/go/lco/17-files.go
+++ codes/languages/go/lco/17-files.go
@@ -3,13 +3,23 @@ package main
 // Just like other langs we can play with only '.txt' files
 // For some other types of files there are specific libraries.
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
 
-const file = "./17_ki_file.txt"
-const content = "This will be going to a file"
+const file string = "./17_ki_file.txt"
+const content string = "This will be going to a file"
+// rememeber walrus operator doesn't work in global and stuff
 
 func main() {
-	
+	file, err := os.Create(file)
+	checkNilError(err)
+	defer file.Close() // VERY VERY IMPORTANT, defered so that I don't forget it later on
+	fmt.Println("Type of 'file': %T", file)
+
+	ioutil.ReadFile(file)
 }
 
 func checkNilError(err error){
