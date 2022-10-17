f1560f20dfe5516010e5e08bab83144cfd8a2883
proffapt <proffapt@pm.me>
2022-10-17 17:54:13 +0530

    update

--- codes/languages/go/lco/13-for.go
+++ codes/languages/go/lco/13-for.go
@@ -6,6 +6,12 @@ func main() {
 	langs := []string{"c", "cpp", "python", "ruby", "rust"}
 	fmt.Println(langs)
 
+	// The NORMAL for
+	
+	for i:=0; i<len(langs); i++ {
+		fmt.Println(langs[i])
+	}
+
 	// The RANGE for
 	for i := range langs { 
 		fmt.Println(langs[i])
@@ -16,4 +22,11 @@ func main() {
 	for _, lang := range langs {
 		fmt.Println(lang)
 	}
+	/*
+		Observation:
+		- If using single variable, you get the index by default
+		- In multivariable, the syntax is INDEX, VALUE
+	*/
+
+
 }
