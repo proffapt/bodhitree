7f492fe1af3e42e9eb8019c85f90b064ef4ef04c
proffapt <proffapt@pm.me>
2022-10-17 18:15:19 +0530

    update

--- codes/languages/go/lco/14-functions.go
+++ codes/languages/go/lco/14-functions.go
@@ -5,9 +5,22 @@ import "fmt"
 func main() {
 	greeting()
 	// NOTE: 'greeting' the function's name is a reference to it.
+	result := adder(4, 7)
+	fmt.Println(result)
 }
 
 func greeting(){
 	fmt.Println("Hewwooo OwO")
 }
+
+		// This indicates the return type of a function, if multiple then enclose them in brackets
+		// 				  ^
+		// 				  |
+func adder(a int, b int) int { 
+// func adder(a int, var b int) int {  --> INVALID
+	return a+b
+}
+
 // order of the funtion's declration doesn't matter in any fuking way possible.
+
+// Anonymous functions and Imediately invoked functions are possible too, we gonna learn about them later
