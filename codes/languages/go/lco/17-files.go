c575cdf50c36e55dea49fab5a0c437b4b19e86f7
proffapt <proffapt@pm.me>
2022-10-17 21:56:44 +0530

    update

--- codes/languages/go/lco/17-files.go
+++ codes/languages/go/lco/17-files.go
@@ -5,21 +5,21 @@ package main
 
 import (
 	"fmt"
-	// "io/ioutil"
+	"io/ioutil"
 	"os"
 )
 
-const file string = "./17_ki_file.txt"
+const filepath string = "./17_ki_file.txt"
 const content string = "This will be going to a file"
 // rememeber walrus operator doesn't work in global and stuff
 
 func main() {
-	file, err := os.Create(file)
+	file, err := os.Create(filepath)
 	checkNilError(err)
 	defer file.Close() // VERY VERY IMPORTANT, defered so that I don't forget it later on
 	fmt.Printf("Type of 'file': %T", file)
 
-	// ioutil.ReadFile(file)
+	ioutil.ReadFile(filepath)
 }
 
 func checkNilError(err error){
