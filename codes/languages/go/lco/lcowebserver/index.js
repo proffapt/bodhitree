3b8b1c3baab855e1992dfcfcdda9bf36352c3146
proffapt <proffapt@pm.me>
2022-10-18 01:11:56 +0530

    update

--- codes/languages/go/lco/lcowebserver/index.js
+++ codes/languages/go/lco/lcowebserver/index.js
@@ -3,29 +3,29 @@ Part of exercise file for go lang course at
 https://web.learncodeonline.in
 */
 
-const express = require(express)
+const express = require('express')
 const app = express()
 const port = 8000
 
 app.use(express.json()); 
 app.use(express.urlencoded({extended: true}));
 
-app.get(/, (req, res) => {
+app.get('/', (req, res) => {
   res.status(200).send("Welcome to LearnCodeonline server")
 })
 
-app.get(/get, (req, res) => {
+app.get('/get', (req, res) => {
     res.status(200).json({message: "Hello from learnCodeonline.in"})
   })
 
 
-app.post(/post, (req, res) => {
+app.post('/post', (req, res) => {
     let myJson = req.body;      // your JSON
-
-res.status(200).send(myJson);
+	
+	res.status(200).send(myJson);
 })
 
-app.post(/postform, (req, res) => {
+app.post('/postform', (req, res) => {
     res.status(200).send(JSON.stringify(req.body));
 })
   
