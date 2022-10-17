f74e7114a3016513c3a8472a4ca7c3c08b9a90a4
proffapt <proffapt@pm.me>
2022-10-17 18:36:29 +0530

    update

--- codes/languages/go/lco/10-structs.go
+++ codes/languages/go/lco/10-structs.go
@@ -1,10 +1,8 @@
 package main
-// no inheritance concept in golang, neither super and parent.
+
 import "fmt"
 
-// Syntax of struct 
-type User struct { // FOCUS ON "User" that capital U has an importance, it means this struct User is publicly avaialable to use
-	// below have same capital letter, which carry the same meaning.
+type User struct {
 	Name string
 	Email string
 	Verified bool
@@ -14,13 +12,4 @@ type User struct { // FOCUS ON "User" that capital U has an importance, it means
 func main() {
 	proffapt := User{"Arpit", "proffapt@pm.me", true, 20}
 	fmt.Println(proffapt)
-	fmt.Printf("%v\n", proffapt)
-
-	// for more detailed output
-	fmt.Printf("%+v\n", proffapt)
-
-	// for individual stuff
-	fmt.Printf("%v\n", proffapt.Age)
-	fmt.Printf("%+v\n", proffapt.Name) // -->> To note, it is same like earlier
-	fmt.Println(proffapt.Email)
 }
