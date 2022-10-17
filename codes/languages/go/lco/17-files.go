37bc4233671c469a9223cdd20cb39385c3ecff14
proffapt <proffapt@pm.me>
2022-10-17 21:44:29 +0530

    update

--- codes/languages/go/lco/17-files.go
+++ codes/languages/go/lco/17-files.go
@@ -5,7 +5,7 @@ import "fmt"
 // Just like other langs we can play with only '.txt' files
 // For some other types of files there are specific libraries.
 func main() {
-	fmt.Println("I love you, Arpit")
+
 }
 
 func checkNilError(err error){
