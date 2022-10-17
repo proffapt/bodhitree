864a634e8a3b8ad8b2b7281d0cd16928d65a1581
proffapt <proffapt@pm.me>
2022-10-17 22:02:03 +0530

    update

--- codes/languages/go/lco/17-files.go
+++ codes/languages/go/lco/17-files.go
@@ -15,16 +15,20 @@ const content string = "This will be going to a file"
 // rememeber walrus operator doesn't work in global and stuff
 
 func main() {
+	// creating and closing a file
 	file, err := os.Create(filepath)
 	checkNilError(err)
 	defer file.Close() // VERY VERY IMPORTANT, defered so that I don't forget it later on
 	fmt.Printf("Type of 'file': %T\n", file)
 
+	// writing into a file
 	io.WriteString(file, content)
 
+	// reading a file
 	databyte, err := ioutil.ReadFile(filepath)
 	checkNilError(err)
 	fmt.Printf("Type of 'databyte': %T\n", databyte)
+	fmt.Println(databyte)
 }
 
 func checkNilError(err error){
