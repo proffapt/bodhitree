9748ce7efba98eb3fa5e42f6262e0cacbc37ab9a
proffapt <proffapt@pm.me>
2022-10-17 22:42:23 +0530

    update

--- codes/languages/go/lco/19-urls.go
+++ codes/languages/go/lco/19-urls.go
@@ -1,9 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/url"
+)
 
 const urlpath string = "https://proffapt.me:6969/eol?user=arpit&session_id=kla45q14"
 
 func main() {
 	fmt.Println("The url is: ", urlpath)
+	
+	// Parsing (extracting information from) the URL
+	url.Parse(urlpath)
 }
