7c013a000bde160d5314ca8b9fbb1d3147cee789
proffapt <proffapt@pm.me>
2022-10-17 22:07:42 +0530

    update

--- codes/languages/go/lco/17-files.go
+++ codes/languages/go/lco/17-files.go
@@ -18,6 +18,7 @@ func main() {
 	// creating and closing a file
 	// VERY VERY IMPORTANT, Close the file using defer at the time of creation only
 	// so that I don't forget it later on
+	fmt.Println("------ File creation phase ------")
 	file, err := os.Create(filepath)
 	checkNilError(err)
 	defer file.Close()
@@ -25,12 +26,14 @@ func main() {
 	fmt.Println("file = ", file)
 
 	// writing into a file
+	fmt.Println("------ File writing phase ------")
 	length, err := io.WriteString(file, content)
 	checkNilError(err)
 	fmt.Printf("Type of 'length': %T\n", length)
 	fmt.Println("length = ", length)
 
 	// reading a file
+	fmt.Println("------ File reading phase ------")
 	databyte, err := ioutil.ReadFile(filepath)
 	checkNilError(err)
 	fmt.Printf("Type of 'databyte': %T\n", databyte)
