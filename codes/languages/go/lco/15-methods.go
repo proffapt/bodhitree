1c258e7b477705b4961a71125dca566c09d5a7ac
proffapt <proffapt@pm.me>
2022-10-17 18:37:50 +0530

    update

---
+++ codes/languages/go/lco/15-methods.go
@@ -0,0 +1,16 @@
+
+package main
+
+import "fmt"
+
+type User struct {
+	Name string
+	Email string
+	Verified bool
+	Age int
+}
+
+func main() {
+	proffapt := User{"Arpit", "proffapt@pm.me", true, 20}
+	fmt.Println(proffapt)
+}
