792603eb073d65e2ad72ce312e56d9dcedfad5a8
proffapt <proffapt@pm.me>
2022-10-17 21:50:05 +0530

    update

--- codes/languages/go/lco/17-files.go
+++ codes/languages/go/lco/17-files.go
@@ -1,11 +1,15 @@
 package main
 
-import "fmt"
-
 // Just like other langs we can play with only '.txt' files
 // For some other types of files there are specific libraries.
-func main() {
 
+import "fmt"
+
+const file = "./17_ki_file.txt"
+const content = "This will be going to a file"
+
+func main() {
+	
 }
 
 func checkNilError(err error){
