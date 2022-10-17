a137a6832d84571fa0742fc82c80c8c5acc776ae
proffapt <proffapt@pm.me>
2022-10-17 18:44:27 +0530

    update

--- codes/languages/go/lco/15-methods.go
+++ codes/languages/go/lco/15-methods.go
@@ -16,7 +16,14 @@ func (u User) getdata(){
 	fmt.Println(u)
 }
 
+func (u *User) setemail(mail string){
+	u.Email = mail
+}
+
 func main() {
 	proffapt := User{"Arpit", "proffapt@pm.me", true, 20}
+	// since methods are functions connected / under structs, they need to be called from object of those structs
+	proffapt.getdata()
+	proffapt.setemail("me@proffapt.me")
 	proffapt.getdata()
 }
