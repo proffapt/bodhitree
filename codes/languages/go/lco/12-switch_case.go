04e4d8eb7fa5427ec8d814e3baedcc0cd36e0e2a
proffapt <proffapt@pm.me>
2022-10-17 17:20:36 +0530

    update

--- codes/languages/go/lco/12-switch_case.go
+++ codes/languages/go/lco/12-switch_case.go
@@ -23,8 +23,7 @@ func main() {
 			fmt.Println("Weee, it's a 4")
 			fallthrough // will fall to the very next case only, NOOOT till the end!
 		case 5:
-			{ 
-				fmt.Println("Weee, it's a 5")
+			{ 	fmt.Println("Weee, it's a 5")
 				fmt.Println("Wait! Did we just have a fallthrough here?") }
 		case 6:
 			fmt.Println("Weee, it's a 6")
