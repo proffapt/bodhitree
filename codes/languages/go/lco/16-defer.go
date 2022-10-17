0dca9ff04ce821ff9ea2c5e5ffc075606cc8ee26
proffapt <proffapt@pm.me>
2022-10-17 19:02:52 +0530

    update

--- codes/languages/go/lco/16-defer.go
+++ codes/languages/go/lco/16-defer.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+// defer == delayed
+// that line of code with defer in the start is placed in the very end of the code, and hence is executed in the very end too.
+// Follows LIFO, logical hai.
 func main() {
-	
+	defer fmt.Println("You")
+	fmt.Println("Fuck")
 }
