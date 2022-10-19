893bfad06bd1154350a38fc4981c6db5dad04aeb
proffapt <proffapt@pm.me>
2022-10-19 15:48:37 +0530

    update

---
+++ codes/languages/go/lco/20-webreqverbs.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+const url string = "http://localhost:8000"
+
+func main() {
+	GetRequest(url)
+}
+
+func GetRequest(url string) {
+	response, err := http.Get(url)
+	cne(err)
+	defer response.Body.Close()
+
+	fmt.Println("Status Code: ", response.StatusCode)
+	fmt.Println("Content Lenght: ", response.ContentLength)
+	databyte, _ := ioutil.ReadAll(response.Body)
+	fmt.Println("Content Lenght: ", string(databyte))
+}
+
+func cne(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
