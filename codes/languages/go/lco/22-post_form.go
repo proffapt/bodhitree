4cf7e1da228430b9202bd9d3c286b92ef5401210
proffapt <proffapt@pm.me>
2022-10-28 21:08:37 +0530

    update

--- codes/languages/go/lco/22-post_form.go
+++ codes/languages/go/lco/22-post_form.go
@@ -1,15 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+)
 
-const url string = "http://localhost:8000/postform"
+const strurl string = "http://localhost:8000/postform"
 
 func main() {
 	postRequestFORM()
 }
 
 func postRequestFORM(){
-	
+	// constructing data
+
+	data := url.Values{}
+
+	data.Add("username", "proffapt")
+	data.Add("password", "mein_nahin_bataunga")
+	data.Add("email", "proffapt@pm.me")
+
+	response, err := http.PostForm(strurl, data)
+	cne(err)
+
+	fmt.Println(response)
 }
 
 func cne(e error) {
