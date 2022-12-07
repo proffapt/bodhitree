cb7095641a606f5f3901923b4aa1c702508b463f
proffapt <proffapt@pm.me>
2022-12-07 23:57:08 +0530

    session 1

---
+++ codes/languages/python/HelloWorld.py
@@ -0,0 +1,30 @@
+# This is a comment in python - resembles to same in other scripting languages.
+
+print("Hello World")
+print(5)
+print("Hellow", 7, 888)
+print(6*5)
+
+# print("I want a 
+#       new line")
+## We can't do like this, it should be in one line.
+print("I want a\nnew line")
+
+'''
+This is a 
+multi line comment in python... wee
+'''
+
+"""
+This is also 
+a multi line comment 
+"""
+
+print("\" hehe")
+
+# sep and end
+
+print("test", 1, 2, 3, sep="~", end="^^^\n")
+print("done")
+# sep:= adds a character used to separate multiple values given in print statement - default = space
+# end:= appends given string into end of the print statement - default = new line (\n)
