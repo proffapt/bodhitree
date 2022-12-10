94918aeb2b3aec6c2f11e0a0f5798ca909935bc6
proffapt <proffapt@pm.me>
2022-12-10 22:20:29 +0530

    update

--- codes/languages/python/002_variables.py
+++ codes/languages/python/002_variables.py
@@ -15,3 +15,7 @@ print(type(f))
 # operations work on same data typed vars.. obviously.. but here's a catch i mean like numbers can be added to numbers
 # so if float is added to int they both still are numers right.. so that works tho the final output is converted into the datatype
 # of higher order.. that's another story..
+
+# collection of datas and can be of different data types - mutable
+list = [8, 6.7, [2, 5], "fuck", True]
+print(list)
