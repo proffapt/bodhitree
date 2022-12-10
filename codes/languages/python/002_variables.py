446535af9876e44c962a0ff41a7efd78da0778e4
proffapt <proffapt@pm.me>
2022-12-10 22:18:00 +0530

    update

--- codes/languages/python/002_variables.py
+++ codes/languages/python/002_variables.py
@@ -2,8 +2,16 @@ a = 1
 b = True
 c = "proff"
 d = None
+e = 2.4
+f = comp(4, 5)
 
 print(type(a))
 print(type(b))
 print(type(c))
 print(type(d))
+print(type(e))
+print(type(f))
+
+# operations work on same data typed vars.. obviously.. but here's a catch i mean like numbers can be added to numbers
+# so if float is added to int they both still are numers right.. so that works tho the final output is converted into the datatype
+# of higher order.. that's another story..
