089aa3cd419c50ed85d974e30bcbaf15f748bc8c
proffapt <proffapt@pm.me>
2022-12-20 23:24:30 +0530

    session 3

--- codes/languages/python/008_list.py
+++ codes/languages/python/008_list.py
@@ -47,3 +47,43 @@ print(list)
 list = [i*i for i in range(10) if i%2==0]
 #                                   ^ condition to filter out the list
 print(list)
+
+##### List methods
+list.append(45) # > appending an entry to the list
+print("Appending 45 to above list:", list)
+list.reverse() # > reverses the list
+print("Reversing above list:", list)
+list.sort() # > sorting the list in ASCENDING order
+print("Sorting above list in ASCENDING order:", list)
+list.sort(reverse=True) # > sorting the list in DESCENDING order
+print("Sorting in DESCENDING order:", list)
+print("Index of `0`:", list.index(0)) # > returns the index of first occurance of the index
+print("Number of occurances of `45`:", list.count(45)) # > counts the number of times 45 has occured
+
+### l is the pointer to the address in memory where the list is
+print(list)
+m = list # > will create another pointer
+m[0]=45
+print("m=", m)
+print("Printing list after altering m:", list) # > This will also be changed because you know... memory pe data hi change kardiya
+## To actually copy a list..
+print(list)
+n = list.copy()
+n[0]=69
+print("n=", n)
+print("Printing list after altering n:", list)
+
+list.insert(1, 69) # > Index 1 par 69 thoos de
+print("Inserting 69 on the index 1:", list)
+
+### Extension of list
+a = ["fuck", "your", "momma"]
+print("a =", a)
+list.extend(a)
+print("Exteding the list by a:", list)
+
+### Concatenation of list
+b = a + list
+print("Concatenated list of a & list:", b)
+### Note that this is not similar to extension.. in extension the real list is modified
+### But in here a new list is created with combination of multiple lists
