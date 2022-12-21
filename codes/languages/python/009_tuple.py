0a9a2cc06099dd8e4a8e37a52baa6ee0a7deccfc
proffapt <proffapt@pm.me>
2022-12-21 23:36:47 +0530

    session 7

---
+++ codes/languages/python/009_tuple.py
@@ -0,0 +1,19 @@
+# Tuple is immutable!
+## What's the fucking use then?
+###~> Sometimes you want a CONSTANT list like a constant variable.. for that exact reason.
+tup = (1, 5, 6, "apple")
+# tup[1]=69 # > Un-changeable!
+print("The tuple is:", tup, "& obviously its type:", type(tup))
+
+### tuple of size 1
+tup1 = (1)
+print("The tuple is:", tup1, "& obviously its type:", type(tup1))
+## ^^ Gives the type as int.
+## Obviously the interpreter is cofused ki whether you have written an integer inside () or tuple.. and obviously it will choose int
+tup2 =(1,) # To tell the interpreter explicitly that this is a tuple of size one not an integer
+print("The tuple is:", tup2, "& obviously its type:", type(tup2))
+
+### positive and negative indexing
+print("tup[-2] =", tup[-2]) # last se 2nd counting starts with 1.. and obviously len(tup)-2
+
+#### And literally every other method is similar as of list.. except for the fact that you can't change the existing tuple.
