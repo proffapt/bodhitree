bbb75821ebd9e58b7aef9173657ada22cffe0259
proffapt <proffapt@pm.me>
2022-12-21 12:33:14 +0530

    session 2

--- codes/languages/go/lco/21-get_request.go
+++ codes/languages/go/lco/21-get_request.go
@@ -18,14 +18,22 @@ func GetRequest(url string) {
 	cne(err)
 	defer response.Body.Close()
 
-	fmt.Println("Status Code: ", response.StatusCode)
-	fmt.Println("Content Length: ", response.ContentLength)
+	fmt.Println("Status:", response.Status)
+	fmt.Println("Status Code:", response.StatusCode)
+	fmt.Println("Protocol :", response.Proto)
+	fmt.Println("Protocol version HTTP/THIS.ignore:", response.ProtoMajor)
+	fmt.Println("Protocol version HTTP/ignore.THIS:", response.ProtoMinor)
+	fmt.Println("Content Length:", response.ContentLength)
+	// This is the method which I might see in some code to just get the data from databyte - "response"
 	var responseString strings.Builder // --> Creating the builder
 	databyte, _ := ioutil.ReadAll(response.Body)
-	responseString.Write(databyte) // Building the string using the builder; databyte as input
-	fmt.Println("Content: ", responseString.String())
+	length, _ := responseString.Write(databyte) // Building the string using the builder; databyte as input
+//    ^  This is general idea that whenever something writes it returns the length of content it wrote in total
+	fmt.Println("Length of content written:", length)
+	fmt.Println("Content:", responseString.String())
+	//                                       ^ This is the reason people like this method because they can convert to whatever format they want without any side-hustle as they hold the raw data in the Builder.
 	// Alternative
-	// fmt.Println("Content: ", string(databyte))
+	fmt.Println("Content:", string(databyte))
 }
 
 func cne(e error) {
