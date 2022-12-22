3697c6939cf87fad715e44e78195b061995b9df6
proffapt <proffapt@pm.me>
2022-12-22 15:20:41 +0530

    session 2

--- codes/languages/go/lco/25-modules/main.go
+++ codes/languages/go/lco/25-modules/main.go
@@ -26,22 +26,19 @@ go mod init github.com/proffapt/modules
 2. Fetch a module from web
 go get -u github.com/gorilla/mux
 
-^ will also to tell the other user that this repo is going to be utilised so better get it
-adds an entry of this module along with exact version
-and for added layer of security via checksum it adds hash to that particular version in `go.sum` file
+^ will also tell the other user that this repo is going to be utilised so better get it
+- Adds an entry of this module along with exact version in `go.mod` file
+- For added layer of security via checksum it adds hash to that particular version in `go.sum` file
 
-	>>> go mod verify >>> Can be used to verify the checksum of package used
+	>>> go mod verify >>> Can be used to verify the checksum of packages used
 
-btw it actually recied in "$GOHOME/pkg/mod/cache/download"
+btw the external packages actually are placed in "$GOHOME/pkg/mod/cache/download"
 
 3. `go mod tidy`
 NOTE will fix the `go.mod` and `go.sum` files
 If something was there but not used -> will be removed after it
+If something was there and also being used but says `//indirect` -> will remove the indirect
 If something wasn't there and was used -> will be added after it
-
-### // indeirect
-This will be there in `go.mod` file with modules which are not used but are there
-after using `go mod tidy` will also handle it accordingly
 
 ******** go mod ... operations are expensive ************
 
@@ -57,7 +54,7 @@ github.com/proffapt/modules
 github.com/gorilla/mux v1.8.0 -> current project, all required modules (external and internal)
 
 ~> go list -m -versions github.com/gorilla/mux
-github.com/gorilla/mux v1.2.0 v1.3.0 v1.4.0 v1.5.0 v1.6.0 v1.6.1 v1.6.2 v1.7.0 v1.7.1 v1.7.2 v1.7.3 v1.7.4 v1.8.0 -> Existing versions of the package to know directly from the terminal and utilise if required
+github.com/gorilla/mux v1.2.0 v1.3.0 v1.4.0 v1.5.0 v1.6.0 v1.6.1 v1.6.2 v1.7.0 v1.7.1 v1.7.2 v1.7.3 v1.7.4 v1.8.0 -> Existing versions of the package to know directly from the terminal and utilise if required (version history)
 
 ~> go mod why -m all
 # github.com/proffapt/modules
@@ -84,14 +81,14 @@ github.com/proffapt/modules github.com/gorilla/mux@v1.8.0
 ### Important observations
   - `-m all` to any command to get information for things being used in the current project only
 
-### Every external package is in cache dir, so if you deply to some server how you gonna do that?
+### Every external package is in cache dir, so if you deploy to some server how you gonna do that?
 ### OR say you wanna modify some bit of the code in the package itself and then use it..
   - Like you can put all the stuff in the project dir and the put it on server
     `go mod vendor`
     ^
     |
     -------- Creates a `vendor` folder just like node_modules with the packages
-  - and now force go to use the things inside the project dir
+  - and now force go to use the things inside the project's dir
     `go run -mod=vendor main.go`
 */
 func main() {
@@ -99,24 +96,25 @@ func main() {
 	// Using gorilla mux - Forget about the details it is here just for the sake of `go mod ...`
 	r := mux.NewRouter()
 	r.HandleFunc("/", homeServer).Methods("GET")
-	// 		      ^ 	  ^
-	// 		  	  |   	  |
-	//  Responding to   What function to call..
-	//   requests in
+	// 		      ^ 	  ^                 ^
+	// 		  	  |   	  |                 |
+	//  Responding to   What function   What kind of
+	//   requests in     to call..  request so that we can respond
 	//     homedir
 
-	// Running a server in golang
+	// Running a server in golang - is literally a one liner command!
 	log.Fatal(http.ListenAndServe(":4000", r)) // log.Fatal logs whenver something goes wrong
-	// 		  		        ^ 	 ^
-	// 		  	            |    |
-	//                     Host  router
-	//                 Hostname:Port
+	// 		  		        		^      ^
+	// 		  	                    |      |
+	//                     		  Host 	 router
+	//                 		Hostname:Port
+	//                By default Hostname=127.0.0.1
 }
 
 func homeServer(w http.ResponseWriter, r *http.Request) {
 	// 				     ^ 					  ^
 	// 					 |   				  |
-	//     to reply to user's request		To get exact request and it's content
-	w.Write([]byte("<h1>Hello from proffapt</h1>"))
-	//         ^ We received everything from the web in []byte format so that means it's the standard so that's what we gonna send
+	//     to reply to user's request	To get exact request and it's content
+	w.Write([]byte(`<h1>Hello from proffapt</h1>`)) // putting string inside []byte within `` will convert it into bytes
+	//         ^ We received everything from the web in []byte format so that means it's the standard so that's exactly what we gonna send
 }
