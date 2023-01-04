f4d1fd602c73aab78ee39e08fdfcd3670662db2b
proffapt <proffapt@pm.me>
2023-01-04 10:34:24 +0530

    session 1

--- codes/languages/python/014_error_handling.py
+++ codes/languages/python/014_error_handling.py
@@ -15,3 +15,19 @@ except ValueError:
     print("Invalid index specified!")
 except IndexError:
     print("Out of bound!")
+
+# Sometimes you require some lines of code to run irrespective of whatever happens
+# Something like cleanup? Closing up data streams and connections etc.
+## That's exactly where "finally" keyword helps us.
+## It's significance is seen when we try something inside a function and return it.
+def fun(i):
+    try:
+        c = [1, 2, 3]
+        i = int(input("Enter an integer:"))
+        return c[i]
+    except:
+        return -1
+    finally:
+        print("This is always executed!") # will still run even if the function has returned.
+
+print(fun(i))
