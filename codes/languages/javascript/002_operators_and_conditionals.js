7e50cc6b57f08ce715a939a233425f4293390e82
proffapt <proffapt@pm.me>
2023-01-05 23:40:01 +0530

    session 3

--- codes/languages/javascript/002_operators_and_conditionals.js
+++ codes/languages/javascript/002_operators_and_conditionals.js
@@ -1,3 +1,4 @@
+const prompt = require("prompt-sync")();
 l = console.log
 // OPERATORS
 /*
@@ -8,9 +9,47 @@ l = console.log
 */
 // I know the basic operators already.. things to note are:
 l(45/4) // gives exact value in float
+// Normal
 l(6 == "6")
 l(6 != "6")
-l(6 === "6") // Triple = does a type check along with value check which double = doesn't!
-l(6 !== "6")
+// New
+// Triple = does a type check along with value check which double = doesn't!
+l(6 === "6") // False
+l(6 !== "6") // True
 
 // CONDITIONALS
+console.clear()
+let a = prompt("Hey what's your age? ")
+l(a, typeof a)
+a = Number.parseInt(a) // Converting a from whatever to Int
+l(a, typeof a)
+
+// If .. else if .. else
+if (a<0){
+	l("Ho bhi tum?")
+}
+else if (a<35){
+	l("Javani chal rahi")
+}
+else {
+	l("Budhapa aa gaya")
+}
+
+// switch - exactly similar to C
+switch (a) { // must wrap arround ()
+	case 0:
+		l("hein?")
+		break // Very important to break.. you know why.. to prevent fall-through
+	case 69:
+		l("holy age acheived")
+	default:
+		l("jhoom jhoom")
+}
+
+// ternary operator
+a = (69 > 79) ? "Sahi baat hai" : "Chutiya gaye ho ka?"
+//  ^expression     ^ Executed when	      ^ Executed when expression is false
+// 				   expression is true
+l(a)
+// OR
+l((69 > 79) ? "Sahi baat hai" : "Chutiya gaye ho ka?")
---
+++ codes/languages/javascript/003_loops.js
@@ -0,0 +1,32 @@
+let l = console.log
+
+// Normal FOR-loop
+for (let i=0; i<5; i++){
+	l(i)
+}
+
+// FOR-IN loop
+/*
+	Iterates over keys for any type of datastructure which is not abstract.
+	For example: Object - Array - String
+*/
+let myobj = {
+	66: 99,
+	55: 55,
+	23: 90,
+	34: 69
+}
+for (key in myobj) { // Again `()` are necessary.
+	l(key)
+}
+
+let array = [1, 2, 3, 4, 5]
+// FOR-OF loop
+/*
+	Iterates over valus for ITERABLE datastructures like - arrays and strings.
+
+	It does NOT lets you iterate of object by default because Symbol.iterator is not there by-default.
+*/
+for (value of array) {
+	l(value)
+}
