6ca304cfc926bf386346ede87e4677581c61516d
proffapt <proffapt@pm.me>
2023-01-12 23:54:42 +0530

    session 1

--- codes/languages/python/filetoimport.py
+++ codes/languages/python/filetoimport.py
@@ -1,3 +1,10 @@
 name='proffapt'
 def welcome(user):
     print("Welcome master,", user)
+
+print("This will also run when this file is imported")
+print(__name__)
+## Above statement will give __main__ when this file is directly executed
+## And when this is imported it will give the name of this file
+if __name__ == "__main__":
+    print("This statement will run only if this file is executed")
