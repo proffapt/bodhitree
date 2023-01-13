72045c3861f28ed2b7cbd0e5e198d3fcb12483de
proffapt <proffapt@pm.me>
2023-01-13 18:43:23 +0530

    session 1

---
+++ codes/languages/javascript/007_scripttag.js
@@ -0,0 +1,10 @@
+// Integrating JS into your project
+/*
+	You have to add a <script> tag in your html file, in two ways:
+		1. Referencing an external file via the `src` attribute
+		2. Writing the JS code right there within the tag.
+	
+	First method is the preferred method for following reasons:
+		- Separation of concerns
+		- Caching; our js file is cached when using this method
+*/
--- codes/languages/python/018_osmodule.py
+++ codes/languages/python/018_osmodule.py
@@ -1,3 +1,15 @@
 import os
 
-print(os.getcwd())
+print("Current working dir:", os.getcwd())
+os.chdir("/Users/proffapt/sandbox/")
+print("Current working dir:", os.getcwd())
+print("Contents of sandbox:", os.listdir("/Users/proffapt/sandbox/"))
+if (not os.path.exists("/Users/proffapt/sandbox/os_dir")):
+    os.mkdir("/Users/proffapt/sandbox/os_dir")
+    # os.mkdir("~/sandbox/os_dir") #-> Won't work, because ~ won't expand here!
+
+print("Contents of sandbox:", os.listdir("/Users/proffapt/sandbox/"))
+os.rename("/Users/proffapt/sandbox/os_dir", "/Users/proffapt/sandbox/osdir")
+print("Contents of sandbox:", os.listdir("/Users/proffapt/sandbox/"))
+os.rmdir("/Users/proffapt/sandbox/osdir")
+print("Contents of sandbox:", os.listdir("/Users/proffapt/sandbox/"))
