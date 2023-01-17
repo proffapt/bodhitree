2972d6d87458fec274a2442d71485212c61c2b28
proffapt <proffapt@pm.me>
2023-01-17 15:06:16 +0530

    session 2

--- codes/languages/javascript/018_callbacks.js
+++ codes/languages/javascript/018_callbacks.js
@@ -26,3 +26,27 @@
 				step 3
 				step 2 after 3 seconds from execution of settimeout
 */
+// Callbacks
+function loadScript(src, callback){
+	let script = document.createElement('script')
+	script.src=src
+	script.onload=()=>{
+		console.log("Script has been loaded successfully!")
+		// What if you wanted to execute a function of your own choice 
+		// instead of this block and that too will be specified on runtime..
+		// We use callbacks for this..
+		callback()
+	}
+	document.body.append(script)
+}
+function hello(){
+	alert("Hello World!")
+}
+loadScript("https://kahin.se.bhi.lelo", hello)
+/*
+	This is called 'callback-based' style of asynchronous programming.
+	A asynchronous function when completes executes a function which signifies the completion of that
+	asynchronous task. That function is called callback function. Here, the completion can be a success or a failure.
+*/
+
+// Handling errors
