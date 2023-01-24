134df6035fd28fc4aa537ac370fc57aba08cc080
proffapt <proffapt@pm.me>
2023-01-24 15:50:09 +0530

    session 3

---
+++ codes/languages/javascript/node.js/038_npm.js
@@ -0,0 +1,19 @@
+console.log("Packages in javascript")
+/*
+	npm init
+		Initialises project, taking input for multiple entries to be stored in `package.json`
+	
+	npm init -y
+		Does the same thing but doesn't ask for what to write, instead uses default/preset values.
+	
+	npm i <package_name>
+		Installs the pacakge & its dependencies
+		For the particular project (folder) where it is executed
+		Everything placed inside `node_modules` directory.
+
+	npm i -g <package_name>
+		Installs the package globally
+
+	npm install
+		Install all the packages mentioned in `pacakge.json` as requirements
+*/
