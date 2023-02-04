8241b8ebb301f57eea25ccd1beae5e97da049e0b
proffapt <proffapt@pm.me>
2023-02-04 11:38:04 +0530

    session 1

--- codes/dsa/abDSAOne/005_mathematics.java
+++ codes/dsa/abDSAOne/005_mathematics.java
@@ -1,6 +1,6 @@
 class Main{
 	public static void main(String[] args){
-		sieveOfEratoSthenes(100);
+		System.out.println(GCD(24, 60));
 	}
 	// Question 1
 	static void trailingZeroesInFactorial(int n){
@@ -39,13 +39,38 @@ class Main{
 	// Question 3 - how many primes from 1 to n?
 	static void sieveOfEratoSthenes(int n){
 		boolean[] isNotPrime = new boolean[n+1];
+		isNotPrime[0]=true; isNotPrime[1]=true; // 0 & 1 are not prime
 		for(int i=2; i*i<=n; i++){
-			for(int j=i*2; j<=n; j+=j/2){
+			for(int j=i*2; j<=n; j+=i){
 				isNotPrime[j]=true;
 			}
 		}
 		for(int i=0; i<=n; i++){
 			if (!isNotPrime[i]) System.out.print(i+" ");
 		}
+	}
+	// Question 4 - Best way to find GCD of two numbers
+	static int GCD(int a, int b){ // Euclidean Algorithm
+		// Theorem: GCD(a, b) = GCD(b, a%b)
+		/*
+			We will exploit this very theorem, and repeat it recursively until one of the value
+			reaches to 0, then the other remaining value will be the required GCD of initial numbers.
+		*/
+		if (a>b) {
+			a=a^b;
+			b=a^b;
+			a=a^b;
+		}
+		// a < b Always! Why?
+		/*
+			Because you are going to do b%a, hence a has to be less than b.
+			Otherwise, it will just keep on running indefinetly with same arguments.
+		*/
+		if (a==0) return b; // Hence, b is never going to be zero first
+		return GCD(b%a, a);
+		/* Better approach, but hard to understand
+			if (b==0) return a; 
+			return GCD(b, a%b);
+		*/
 	}
 }
