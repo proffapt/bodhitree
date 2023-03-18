bb84db8fcc836851f8ffb7478b28cf32256654ac
proffapt <proffapt@pm.me>
2023-03-18 20:11:55 +0530

    session 1

--- codes/dsa/abDSAOne/008_recursion_and_strings.java
+++ codes/dsa/abDSAOne/008_recursion_and_strings.java
@@ -3,7 +3,18 @@ class Main{
 	public static void main(String[] args){
 		// System.out.println(isPalindrome("fuck", 0, "fuck".length()-1));
 		// System.out.println(isPalindrome("racecar", 0, "racecar".length()-1));
-		System.out.println(powerSet("abc", 0, "abc".length()-1));
+		powerSet("abc", 0, "");
+	}
+
+	// Powerset of a string, All the substrings which can be made from the given string
+	static void powerSet(String s, int i, String cur){
+		if (i==s.length()){
+			System.out.print(cur+" ");
+			return;
+		}
+
+		powerSet(s, i+1, cur);
+		powerSet(s, i+1, cur+s.charAt(i));
 	}
 	// Whether a given string is a palindrome or not
 	static boolean isPalindrome(String str, int l, int r){
@@ -14,14 +25,5 @@ class Main{
 			return isPalindrome(str, l+1, r-1);
 		else 
 			return false;
-	}
-	// Powerset of a string, All the substrings which can be made from the given string
-	static String powerSet(String s, int l, int r){
-		if (l>r){
-			System.out.println("");
-			return "\nDONE";
-		}
-		System.out.println(s.substring(l, r));
-		return s.charAt(l) + powerSet(s, l+1, r);
 	}
 }
