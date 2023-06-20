92fa85b55fc3af8083020ce48132f367f4abe906
proffapt <proffapt@pm.me>
2023-06-20 20:57:56 +0530

    session 2

--- codes/dsa/abDSAOne/048_bt_questions_2.java
+++ codes/dsa/abDSAOne/048_bt_questions_2.java
@@ -8,8 +8,37 @@ class Main {
 
     // Q6 - Left view of a binary tree
     // Very fundamental changes for getting the left view, more or less similar
+    System.out.print("Left View of Binary Tree = ");
     qSix(bt);
     System.out.println();
+
+    // Q7 - Top view of binary tree
+    System.out.print("Top View of Binary Tree = ");
+    qSeven(bt);
+    System.out.println();
+  }
+
+  static void qSeven(Node root){
+    if(root == null) return;
+    
+    // Constructing the map first
+    Map<Integer, Node> map = inOrder(root, 0);
+    
+    map.forEach((hd, node) -> System.out.print("(" + hd + "," + node.data + ") "));
+  }
+
+  // Doing InOrder Traversal, creating the map
+  static Map<Integer, Node> inOrder(Node root, int hd){
+    if(root == null) return null;
+    
+    Map<Integer, Node> map = new HashMap<>();
+    
+    inOrder(root.left, hd-1);
+    System.out.println(root.data);
+    map.put(hd, root);
+    inOrder(root.right, hd+1);
+
+    return map;
   }
   
   // remove `<-` lines and you will get right view of the binary tree
